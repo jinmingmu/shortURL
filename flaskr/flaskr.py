cf933b0b1b1eb5ad571d5dcc83ebc357f67edcf1
Jinming Mu <Jinming.Mu@Cerner.com>
2017-02-23 10:25:49 -0600

    return json file instead of strings and abort

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 from encode.base import *
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, abort
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, abort, jsonify
 import re
 app = Flask(__name__)
 
@@ -19,29 +19,29 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
-	
+
 def get_db():
     """Opens a new database connection if there is none yet for the
     current application context.
     """
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = connect_db()
-    return g.sqlite_db	
-	
-	
+    return g.sqlite_db
+
+
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()	
-	
+
 def init_db():
     with app.app_context():
         db = get_db()
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
-		
+
 @app.cli.command('initdb')
 def initdb_command():
     """Creates the database tables."""
@@ -49,20 +49,34 @@ def initdb_command():
     print('Initialized the database.')
     
 @app.route('/')
-def postCenter():
+def main_page():
     addLongURL = request.args.get('add')
     print addLongURL
-    if(addLongURL != None and isValidURL(addLongURL)):
-        return addLongUrl(addLongURL)
+    if(addLongURL != None and is_valid_URL(addLongURL)):
+        shortURL = add_longUrl(addLongURL)
+        response = jsonify(
+            status=200,
+            shortURL=shortURL,
+            mimetype='application/json'
+        )
+        return response
 
     countLongURL = request.args.get('count')
-    print countLongURL
-    if(countLongURL != None and isValidURL(countLongURL)):
-        return getCounter(countLongURL)
+    if(countLongURL != None and is_valid_URL(countLongURL)):
+        counter = get_counter(countLongURL)
+        response = jsonify(
+            status=200,
+            counter=counter,
+            mimetype='application/json'
+        )
+        return response
 
-    abort(404)
+    return jsonify(
+            status=400,
+            error='Bad request'
+            )
 
-def addLongUrl(longURL):
+def add_longUrl(longURL):
     db = get_db()
     contents = db.execute('select * from URLTable where longURL=(?)',[longURL])
     content = contents.fetchall()
@@ -72,9 +86,9 @@ def addLongUrl(longURL):
         db.execute('insert into URLTable (longURL, counter) values(?,?)',[longURL, 0])
         id = db.execute('select id from URLTable where longURL=(?)',[longURL]).fetchall()[0][0]
         db.commit()
-    return url_for('postCenter',_external=True) + encode(id)
+    return url_for('main_page',_external=True) + encode(id)
 
-def getCounter(longURL):
+def get_counter(longURL):
     db = get_db()
     contents = db.execute('select counter from URLTable where longURL=(?)',[longURL])
     db.commit()
@@ -87,7 +101,7 @@ def getCounter(longURL):
     
     
 @app.route('/<hashValue>')
-def parseURL(hashValue):
+def parse_URL(hashValue):
     for char in hashValue:
         if(char not in BASE62):
             abort(404)
@@ -101,15 +115,16 @@ def parseURL(hashValue):
         counter = content[0][2]
         db.execute('update URLTable set counter = (?) where id=(?)',[counter + 1, entryId])
         db.commit()
-        return redirect(longURL)  
+        return redirect(longURL)
     else:
         abort(404)
 
-def isValidURL(url):
+def is_valid_URL(url):
     if re.match(r'^https?:/{2}\w.+$', url):
         return True
     else:
         return False
+
 
 if __name__ == '__main__':
     app.run()
